a4658b9dc68435b8f06c99d82806ae3d0fb38cd9
Kashif Shah <kashif900900@gmail.com>
2024-05-30 08:37:03 +0500

    add new code

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const port = process.env.PORT || 3000
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('welcome')
+})
+
+app.listen(port, () => {
+    console.log(`server at http://localhost:${port}`)
+})
+
+
+
