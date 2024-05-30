acb09e803db9cf46f70013b91db7f0c0b8036fb8
Kashif Shah <kashif900900@gmail.com>
2024-05-30 11:25:38 +0500

    learn professional project setting

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
-import express from 'express'
+import express from "express";
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-    res.send('welcome')
-})
+app.get("/", (req, res) => {
+  res.send("welcome");
+});
 
 app.listen(port, () => {
-    console.log(`server at http://localhost:${port}`)
-})
-
-
-
+  console.log(`server at http://localhost:${port}`);
+});
